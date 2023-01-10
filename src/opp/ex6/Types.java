d65b3b896d70f95d4ea9cdea2db285600aab6273
itayyamin <itayyamin1@gmail.com>
2023-01-10 18:31:16 +0200

    test2

--- src/opp/ex6/Types.java
+++ src/opp/ex6/Types.java
@@ -1,4 +1,10 @@
 package opp.ex6;
 //shalom alichem
 public enum Types {
+    INT,
+    LONG,
+    DOUBLE,
+    FLOAT,
+    BOOLEAN,
+    CHAR
 }
--- src/opp/ex6/main/Sjavac.java
+++ src/opp/ex6/main/Sjavac.java
@@ -1,16 +1,59 @@
 package opp.ex6.main;
 
 
+import java.io.*;
+import java.nio.file.FileSystemAlreadyExistsException;
+
 public class Sjavac {
     public static void main(String[] args) {
 //        TODO: maybe args 1 ? 
-        int result = readFileAndValidate(args[0]);
-        System.out.println(result);
+//        int result = readFileAndValidate(args[0]);
+//        System.out.println(result);
+        readFileAndValidate("C:\\Users\\itayy\\Desktop\\java projects\\oop_ex6\\src\\opp\\ex6\\main\\basic_text_file.txt");
     }
 
-    private static int readFileAndValidate(String file){
-        // read all the file and only scan for variables and methods. 
-        int isMethodsAndVariablesValid = readMethodsAndVariables(file);
+
+//    private static String();
+
+
+    private static int readFileAndValidate(String fileNamePath) {
+
+        FileReader file = null;
+        try {
+            file = new FileReader(fileNamePath);
+        } catch (FileNotFoundException e) {
+            //todo: some breakpoint to prevent from continue and add informative massage
+            System.out.println("File Not Found");
+        }
+        //todo: file.exists?
+        if (file != null) {
+            System.out.println("File opened successfully");
+            //todo: some breakpoint to prevent from continue and add informative massage
+        } else {
+            System.out.println("File open failed");
+        }
+        BufferedReader reader = new BufferedReader(file);
+        String line;
+        try {
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            //todo: some breakpoint to prevent from continue and add informative massage
+            System.out.println("Error reading file");
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                //todo: some breakpoint to prevent from continue and add informative massage
+                System.out.println("Error closing file");
+            }
+        }
+
+
+        return 1;
+//        int isMethodsAndVariablesValid = readMethodsAndVariables(file);
+        // read all the file and only scan for variables and methods.
 //        if(isMethodsAndVariablesValid==0)
 //    // read in buffer from the file:
 //        // for each buffer do:
@@ -18,6 +61,8 @@ public class Sjavac {
     }
 
     private static int readMethodsAndVariables(String file) {
+        System.out.println("");
+        return 1;
         // read ass buffer
         // if space line ->> skip
         // if comments line ->> validate and skip
